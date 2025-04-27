4ebab341363fa562e1292183aab9f60a80a862e7
John Phillips <johnphillips114@gmail.com>
2025-04-27 13:27:56 -0600

    fixed typo

--- noun.h
+++ noun.h
@@ -1,6 +1,8 @@
 #ifndef NOUN_H
 #define NOUN_H
 
+#include <wchar.h>
 extern OBJECT *getVisible(const char *intention, const char *noun);
+extern OBJECT *getPossession(OBJECT *from, const char *verb, const char *noun);
 
 #endif
