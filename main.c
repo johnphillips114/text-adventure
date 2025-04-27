ca3820ee55115287ef2fc4490f969624ee520c7e
John Phillips <johnphillips114@gmail.com>
2025-04-27 08:02:01 -0600

    Added file to parse and execute user input commands

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include "parsexec.h"
+
+static char input[100] = "look around";
+
+static bool getInput(void) {
+    printf("\n--> ");
+    return fgets(input, sizeof input, stdin) != NULL;
+}
 
 int main(void) {
     printf("Welcome to Litle Cave Adventure.\n");
-    printf("It is very dark in here.\n");
+    while (parseAndExecute(input) && getInput());
     printf("\nBye!\n");
     return 0;
 }
