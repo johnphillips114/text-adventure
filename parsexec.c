6b3ce6431872b68de676f441a8c37f1355952249
John Phillips <johnphillips114@gmail.com>
2025-04-27 09:00:39 -0600

    added location file, added prevention for infinate header cycling

--- parsexec.c
+++ parsexec.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include "location.h"
 
 bool parseAndExecute(char *input) {
     char *verb = strtok(input, " \n");
@@ -10,10 +11,10 @@ bool parseAndExecute(char *input) {
             return false;
         }
         else if (strcmp(verb, "look") == 0) {
-            printf("It is very dark in here.\n");
+            executeLook(noun);
         }
         else if (strcmp(verb, "go") == 0) {
-            printf("It's too dark to go anywhere.\n");
+            executeGo(noun);
         }
         else {
             printf("I don't know how to '%s'.\n", verb);
